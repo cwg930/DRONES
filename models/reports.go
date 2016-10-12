01ced32e42f2d8e7686421733290613755320746
Chris <cwg930@gmail.com>
2016-10-12 15:57:47 -0400

    added database interactions for report headers

--- models/reports.go
+++ models/reports.go
@@ -13,3 +13,55 @@ type Entry struct{
 	FileName string `json:"filename"`
 	ID int `json:"id"`
 }
+
+func (db *DB) GetReport(id int) (*Report, error) { 
+	report := &Report{}
+	err := db.QueryRow("SELECT * FROM reports WHERE id = ?", id).Scan(&report.ID, &report.Name, &report.OwnerID, &report.PlanID)
+	if err != nil {
+		return nil, err
+	}
+	return report, nil
+} 
+
+func (db *DB) AllReportsForUser(userId int) ([]*Report, error) {
+	rows, err := db.Query("SELECT * FROM reports WHERE owner = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	reports := make([]*Report, 0)
+	for rows.Next() {
+		report := new (Report)
+		err := rows.Scan(&report.ID, &report.Name, &report.OwnerID, &report.PlanID)
+		if err != nil {
+			return nil, err
+		}
+		reports = append(reports, report)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
+func (db *DB) AddReport(report Report) (int, error) {
+	stmt, err := db.Prepare("INSERT INTO reports(name, owner, plan) VALUES (?,?,?)")
+	if err != nil {
+		return nil, err
+	}
+	res, err := stmt.Exec(report.Name, report.OwnerID, report.PlanID)
+	if err != nil {
+		return nil, err
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Added report: %s with ID %d. Rows affected: %d", report.Name, lastID, rowCnt)
+	return lastID, nil
+}
